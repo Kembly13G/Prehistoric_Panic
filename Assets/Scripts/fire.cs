ec5c7e45cd27cc936addbb341e5b4d9e483311b2
lxlguzmanlxl <92770108+lxlguzmanlxl@users.noreply.github.com>
2023-04-27 01:34:47 -0600

    enemigos finales
    enemigos finales

--- Assets/Scripts/fire.cs
+++ Assets/Scripts/fire.cs
@@ -5,37 +5,17 @@ using UnityEngine.AI;
 
 public class fire : MonoBehaviour
 {
-    
-    public float hideTime = 2.0f; // tiempo en segundos que el objeto se ocultará
-
-   
-   private NavMeshAgent navAgent;
-
-
-  public Transform[] waypoints;
-    public float speed = 5.0f;
-
-   
-
+    private NavMeshAgent navAgent;
+    public Transform[] waypoints;
     int waypointIndex;
-
-
-
-    public float countdownTime = 6f;
+    public float countdownTime = 8f;
     Vector3 target;
+
     void Start()
-    {
-        gameObject.SetActive(false);
-        
+    {       
        navAgent = GetComponent<NavMeshAgent>();
         UpdateDestination();
-        Invoke("ShowObject", hideTime);
     }
-
-    void ShowObject()
-{
-    gameObject.SetActive(true); // mostrar el objeto de nuevo
-}
     void Update()
     {
         if(Vector3.Distance(transform.position, target) < 1)
